e66b3c2ccb537152522960504a9fd91b8aaa6b16
Sithiphat <sithiphatk@gmail.com>
2023-01-31 13:28:24 +0700

    spring boot init

---
+++ src/main/java/tableReserve/TableReserveApplication.java
@@ -0,0 +1,13 @@
+package tableReserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tableReserve/TableReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package tableReserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
