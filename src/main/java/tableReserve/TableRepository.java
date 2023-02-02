5d537484f76f9a4f8ebee5f5dc660d77e6d267f8
Sithiphat <sithiphatk@gmail.com>
2023-02-02 10:52:48 +0700

    Add findByReservedFalseOrderById function in repository

--- src/main/java/tableReserve/TableRepository.java
+++ src/main/java/tableReserve/TableRepository.java
@@ -1,9 +1,13 @@
 package tableReserve;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface TableRepository extends JpaRepository<diningTable,Integer> {
 
+import java.util.List;
+
+@Repository
+public interface TableRepository extends JpaRepository<DiningTable,Integer> {
+    List<DiningTable> findByReservedFalseOrderById(Pageable pageable);
 }
