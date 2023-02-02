9b254f7f3fc750bf42c3cfc66c0b58741ae1d04c
Sithiphat <sithiphatk@gmail.com>
2023-02-02 10:46:52 +0700

    rename Class

--- src/main/java/tableReserve/DiningTable.java
+++ src/main/java/tableReserve/DiningTable.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class diningTable {
+public class DiningTable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
@@ -29,7 +29,7 @@ public class diningTable {
         return personCount;
     }
 
-    public diningTable() {
+    public DiningTable() {
 
     }
 
